49218b5911d968ddd13c22fcd6c504fb8798a82a
phen0menon <roland.ibr@yahoo.com>
2019-09-01 14:24:01 +0300

    Update yarnlock file

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, Children } from "react";
+
+interface PageProps {
+  title: string;
+  children: any;
+}
+
+export default ({ title, children }: PageProps) => {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+  return children;
+};
